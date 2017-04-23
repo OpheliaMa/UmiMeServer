6e7d880aaa12aeeb8287edb7b0ee54dd4f112b20
xuyanjun <xuyanjun@didichuxing.com>
2017-04-24 02:01:31 +0800

    * mail send

---
+++ mailUtil.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"strings"
+	"net/smtp"
+	"fmt"
+	"encoding/base64"
+)
+
+func SendToMail(user, password, host, to, subject, body, mailtype string) error {
+
+	hp := strings.Split(host, ":");
+	auth := smtp.PlainAuth("", user, password, hp[0]);
+
+	header := make(map[string]string)
+	header["From"] = user
+	header["To"] = to
+	header["Subject"] = subject
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = "text/plain; charset=\"utf-8\""
+	header["Content-Transfer-Encoding"] = "base64"
+	header["X-Priority"] = "3"
+	header["X-MSMail-Priority"] = "Normal"
+	header["X-Mailer"] = "Microsoft Outlook Express 6.00.2900.2869"; //本文以outlook名义发送邮件，不会被当作垃圾邮件
+	header["X-MimeOLE"] = "Produced By Microsoft MimeOLE V6.00.2900.2869";
+	header["ReturnReceipt"] = "1";
+
+	message := ""
+	for k, v := range header {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
+
+	send_to := strings.Split(to, ";")
+
+	err := smtp.SendMail(host, auth, user, send_to, []byte(message))
+	return err
+}
--- main.go
+++ main.go
@@ -4,4 +4,27 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello UmiMe!");
+
+	user := "332114994@qq.com"
+	password := "uyblohfooprocbed"
+	host := "smtp.qq.com:587"
+	to := "j.xuyanjun@icloud.com;ophelia.ma@icloud.com"
+
+	subject := "A Mail From Go Server."
+
+	body := `
+		您好：
+			您收到一次报名提交，姓名为：[马玥]，联系方式为[15800705345]，请及时联系。
+
+								      From Go Server.
+		`;
+
+	fmt.Println("send email")
+	err := SendToMail(user, password, host, to, subject, body, "text")
+	if err != nil {
+		fmt.Println("Send mail error!")
+		fmt.Println(err)
+	} else {
+		fmt.Println("Send mail success!")
+	}
 }
